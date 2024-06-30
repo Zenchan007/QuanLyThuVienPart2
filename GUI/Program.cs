709da95f57900fc058537fbbec24c89509888b07
DESKTOP-BQTQ88M\zenchan007 <tuannguyenvan299002@gmail.com>
2024-06-30 12:26:23 +0700

    Xóa các file không cần thiết =)) Nhưng mất bà database rồi :D

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -1,17 +1,6 @@
 ﻿using DevExpress.XtraEditors;
-using GUI.Form_BaoCao;
-using GUI.Form_DocGia;
-using GUI.Form_NhanVien;
-using GUI.Form_NhaPhanPhoi;
-using GUI.Form_PhieuMuon;
-using GUI.Form_Sach;
-using GUI.Form_TacGia;
-using GUI.Form_TheLoai;
 using GUI.Login;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GUI
